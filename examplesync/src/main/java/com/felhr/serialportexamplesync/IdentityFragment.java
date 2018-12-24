46e743db259c86a2fe4b465730009b6a6451db1b
Lucas <lucassusanto@gmail.com>
2018-12-24 20:30:28 +0700

    Add services

--- examplesync/src/main/java/com/felhr/serialportexamplesync/IdentityFragment.java
+++ examplesync/src/main/java/com/felhr/serialportexamplesync/IdentityFragment.java
@@ -6,11 +6,33 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class IdentityFragment extends Fragment {
+    private EditText txMyName;
+    private Button btnChange;
+    private TextView txMyLat;
+    private TextView txMyLon;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_identity, container, false);
+        View view = inflater.inflate(R.layout.fragment_identity, container, false);
+
+        txMyName = view.findViewById(R.id.txMyName);
+        btnChange = view.findViewById(R.id.btnChange);
+        txMyLat = view.findViewById(R.id.txMyLat);
+        txMyLon = view.findViewById(R.id.txMyLon);
+
+        btnChange.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return view;
     }
 }

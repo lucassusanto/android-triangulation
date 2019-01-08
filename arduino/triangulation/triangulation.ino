275b9536c79762efd9a4414d77e5d99445199bf5
Lucas <lucassusanto@gmail.com>
2019-01-09 03:09:59 +0700

    change code to follow pseudocode

--- arduino/triangulation/triangulation.ino
+++ arduino/triangulation/triangulation.ino
@@ -2,8 +2,9 @@
 #include <nRF24L01.h>
 #include <RF24.h>
 
-char buff[32], index = 0; // Internal INPUT
-char buffExternal[32]; // External INPUT
+// Internal & External Input
+char internalBuff[32], index = 0, internalInputFlag = 0;
+char externalBuff[32];
 
 // Radio
 RF24 radio(7, 8); // CE, CSN
@@ -11,8 +12,8 @@ const byte address[6] = "00001";
 int broadcastDelay = 3000, currentDelay = 0;
 
 // My Device Info
-char myDevice[4] = "D1\0";
-float myLatitude = 0, myLongitude = 0;
+char deviceName[4] = "D1\0";
+float deviceLatitude = 0, deviceLongitude = 0;
 
 void setup() {
   Serial.begin(9600);
@@ -32,107 +33,119 @@ void setup() {
 void loop() {
   if(currentDelay >= broadcastDelay) {
     radio.stopListening(); 
-    broadcastMyPosition();
+    broadcast();
     radio.startListening(); 
     
     currentDelay = 0;
   }
   
-  getInputs();
+  getInternalInput();
+  getExternalInput();
 
   delay(1);
   currentDelay++;
 }
 
-void getInputs() {
-  // Get Internal Inputs
+void getInternalInput() {
+  // Read User Input
   if(Serial.available()) {
     while(Serial.available()) {
       char ch = Serial.read();
-  
+      
       if(index < 30 && (ch != ';')) {
-        buff[index++] = ch;
-      } else {
-        if(ch != ';') buff[index++] = ch;
-        buff[index] = 0;
-        
-        processInternalInput();
-        
+        internalBuff[index++] = ch;
+      }
+      else {
+        if(ch != ';') internalBuff[index++] = ch;
+        internalBuff[index] = 0;        
         index = 0;
+        internalInputFlag = 1;
+        break;
       }
     }
   }
+
+  // Process User Input
+  if(internalInputFlag == 1) {
+    if(strncmp(internalBuff, "GD", 2) == 0) {
+      getDeviceName();
+    }
+    else if(strncmp(internalBuff, "SD", 2) == 0) {
+      setDeviceName();
+    }
+    else if(strncmp(internalBuff, "GP", 2) == 0) {
+      getDevicePosition();
+    }
+    else if(strncmp(internalBuff, "SP", 2) == 0) {
+      setDevicePosition();
+    }
+    // Unknown Command
+    else {
+      Serial.println("ERR");
+    }
   
-  // Get External Inputs
-  else if (radio.available()) {    
-    while (radio.available()) radio.read(&buffExternal, sizeof(buffExternal));
-    processExternalInput();
+    internalInputFlag = 0;
   }
 }
 
-void processInternalInput() {
-  // Set Device name
-  if(strncmp(buff, "SD", 2) == 0) {
-    sscanf(buff + 3, "%s", myDevice);    
-    Serial.println("OK");
-  }
-  
-  // Get Device Name
-  else if(strncmp(buff, "GD", 2) == 0) {
-    Serial.println(myDevice);
-  }
-  
-  // Set Latitude and Longitude
-  else if(strncmp(buff, "SP", 2) == 0) {
-    String lat, lon;
-    char blat[13], blon[13];
-    
-    sscanf(buff + 3, "%s %s", blat, blon);
-    
-    myLatitude = String(blat).toFloat();
-    myLongitude = String(blon).toFloat();
-    
-    Serial.println("OK");
-  }
-  
-  // Get Latitude and Longitude
-  else if(strncmp(buff, "GP", 2) == 0) {
-    Serial.print(myLatitude, 6);
-    Serial.print(" ");
-    Serial.println(myLongitude, 6);
-  }
+void getDeviceName() {
+  Serial.println(deviceName);
+}
 
-  // Unknown Command
-  else {
-    Serial.println("ERR");
+void setDeviceName() {
+sscanf(internalBuff + 3, "%s", deviceName);    
+  Serial.println("OK");
+}
+
+void getDevicePosition() {
+  Serial.print(deviceLatitude, 6);
+  Serial.print(" ");
+  Serial.println(deviceLongitude, 6);
+}
+
+void setDevicePosition() {
+  String lat, lon;
+  char blat[13], blon[13];
+  
+  sscanf(internalBuff + 3, "%s %s", blat, blon);
+  
+  deviceLatitude = String(blat).toFloat();
+  deviceLongitude = String(blon).toFloat();
+  
+  Serial.println("OK");
+}
+
+void getExternalInput() {  
+  // Read External Input
+  if (radio.available()) {
+    while (radio.available()) radio.read(&externalBuff, sizeof(externalBuff));
+  
+    // Process External Input
+    if(strncmp(externalBuff, "TRI", 3) == 0) {
+      char fDevice[4], fLatitude[12], fLongitude[12];
+
+      sscanf(externalBuff + 4, "%s %s %s", fDevice, fLatitude, fLongitude);
+      
+      Serial.print("nRF24L01< ");
+      Serial.print(fDevice);
+      Serial.print(" ");
+      Serial.print(fLatitude);
+      Serial.print(" ");
+      Serial.println(fLongitude);
+    }
   }
 }
 
-// Get position data from other devices
-void processExternalInput() {  
-  if(strncmp(buffExternal, "TRI", 3) == 0) {
-    char fDevice[4], fLatitude[12], fLongitude[12];
-    
-    sscanf(buffExternal + 4, "%s %s %s", fDevice, fLatitude, fLongitude);
-    
-    Serial.print("nRF24L01< ");
-    Serial.print(fDevice);
-    Serial.print(" ");
-    Serial.print(fLatitude);
-    Serial.print(" ");
-    Serial.println(fLongitude);
-  }
-}
-
-// Send position data to other devices
-void broadcastMyPosition() {
-  if(myLatitude == 0 && myLongitude == 0) return;
+// Broadcast Our Information
+void broadcast() {
+  if(deviceLatitude != 0 && deviceLongitude != 0) {
+    char myInfo[32];
+    sprintf(myInfo, "TRI %s %s %s\0", deviceName, String(deviceLatitude, 6).c_str(), String(deviceLongitude, 6).c_str());
   
-  char myInfo[32];
-  sprintf(myInfo, "TRI %s %s %s\0", myDevice, String(myLatitude, 6).c_str(), String(myLongitude, 6).c_str());
-
-  radio.write(&myInfo, sizeof(myInfo));
-
-  Serial.print("nRF24L01> ");
-  Serial.println(myInfo + 4);
+    // Broadcast
+    radio.write(&myInfo, sizeof(myInfo));
+  
+    Serial.print("nRF24L01> ");
+    Serial.println(myInfo + 4);
+  }
 }
--- examplesync/src/main/java/com/felhr/serialportexamplesync/views/CustomView.java
+++ examplesync/src/main/java/com/felhr/serialportexamplesync/views/CustomView.java
@@ -22,8 +22,6 @@ import java.util.List;
 import static java.math.BigDecimal.ROUND_HALF_UP;
 
 public class CustomView extends View {
-     WeakReference m; // DEBUG
-
     // Scaling Variables
     double topleftY, topleftX, bottomleftY, toprightX;
     double scaleX, scaleY;
@@ -64,8 +62,6 @@ public class CustomView extends View {
     }
 
     private void init(Context context, @Nullable AttributeSet set) {
-         m = new WeakReference(context); // DEBUG
-
         // Set Paints
         mPaintRef = new Paint(Paint.ANTI_ALIAS_FLAG);
         mPaintRef.setColor(Color.RED);
@@ -77,17 +73,21 @@ public class CustomView extends View {
         mPaintDevices.setColor(Color.BLUE);
 
         // Set References Position
-        refList = new ArrayList<Device>();
-
-        refList.add(new Device("REF1", -7.274922, 112.800401));
-        refList.add(new Device("REF2", -7.283147, 112.790550));
-        refList.add(new Device("REF3", -7.286468, 112.804709));
+        initReferences();
 
         // Set My Position
         myDevice = new Device("null", 0.0, 0.0);
 
         // Set Devices Position
         devicesList = new ArrayList<Device>();
+    }
+
+    private void initReferences() {
+        refList = new ArrayList<Device>();
+
+        refList.add(new Device("REF1", -7.274922, 112.800401));
+        refList.add(new Device("REF2", -7.283147, 112.790550));
+        refList.add(new Device("REF3", -7.286468, 112.804709));
     }
 
     @Override
@@ -127,11 +127,7 @@ public class CustomView extends View {
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-
         canvas.drawBitmap(mImage, 0, 0, null);
-
-//        drawRadius(canvas); // DEBUG
-//        drawIntersectionPoints(canvas); // DEBUG
 
         drawPins(canvas, refList, mPaintRef);
         drawPins(canvas, devicesList, mPaintDevices);
@@ -149,100 +145,32 @@ public class CustomView extends View {
         }
     }
 
-    // DEBUG
-    /*
-    Device dummyDev1;
-    Device dummyDev2;
-    double[] drawRadius = new double[3];
-    */
-
     // Main Calculation
     private Device getTriangulatedPosition(Device device) {
-        int refCount = refList.size();
+        double[] dist = getDistances(device.getLongitude(), device.getLatitude());
 
-        double[]
-            dist = new double[3],
-            rLon = new double[3],
-            rLat = new double[3];
+        Point[] points = getIntersectionPoints(
+                refList.get(0).getLongitude(), refList.get(0).getLatitude(),dist[0],
+                refList.get(1).getLongitude(), refList.get(1).getLatitude(),dist[1]);
 
-        double
-            dLon = device.getLongitude(),
-            dLat = device.getLatitude();
-
-        // Calc Distances
-        for(int i = 0; i < refCount; i++) {
-            Device tmp = refList.get(i);
-
-            rLon[i] = tmp.getLongitude();
-            rLat[i] = tmp.getLatitude();
-
-            dist[i] = distance(rLon[i], rLat[i], dLon, dLat);
-            // drawRadius[i] = dist[i]; // DEBUG
-        }
-
-        // Calc Device Point
-        Point point = calcPoint(
-            rLon[0], rLat[0], dist[0],
-            rLon[1], rLat[1], dist[1],
-            rLon[2], rLat[2], dist[2]);
+        Point point = getTruePoint(points, refList.get(2).getLongitude(), refList.get(2).getLatitude(), dist[2]);
 
         // Return
-        Device newDevice = new Device(device.getName(), point.y, point.x);
-        return newDevice;
+        device.setLatitude(point.y);
+        device.setLongitude(point.x);
+
+        return device;
     }
 
-    // Get True Point
-    private Point calcPoint(double x1, double y1, double r1,
-                          double x2, double y2, double r2,
-                          double x3, double y3, double r3) {
+    // Distance between (x, y) to all reference points
+    private double[] getDistances(double x, double y) {
+        double[] dist = new double[3];
 
-        // Calc intersection points
-        double
-            a = x1 * x1 - x2 * x2 - r1 * r1 + r2 * r2 + y1 * y1 - y2 * y2,
-            b = 2 * (x1 - x2),
-            c = y2 - y1,
-            bPow = Math.pow(b, 2),
-            cPow = Math.pow(c, 2),
-            d = bPow + 4 * cPow,
-            dd = 1 / (2 * d),
-            e = 4 * a * c,
-            f = 2 * bPow * y1,
-            g = 4 * b * c * x1,
-            h = 2 * a * b * x1;
+        dist[0] = distance(x, y, refList.get(0).getLongitude(), refList.get(0).getLatitude());
+        dist[1] = distance(x, y, refList.get(1).getLongitude(), refList.get(1).getLatitude());
+        dist[2] = distance(x, y, refList.get(2).getLongitude(), refList.get(2).getLatitude());
 
-        double
-            y_1 = dd * (Math.sqrt(Math.pow((e - f - g), 2) - 4 * d *
-                (Math.pow(a, 2) - h - bPow * Math.pow(r1, 2) + bPow * Math.pow(x1, 2) + bPow * Math.pow(y1, 2)))
-                - e + f + g),
-            x_1 = calcX(y_1, x1, x2, y1, y2, r1, r2),
-
-            y_2 = dd * (-Math.sqrt(Math.pow((e - f - g), 2) - 4 * d *
-                (Math.pow(a, 2) - h - bPow * Math.pow(r1, 2) + bPow * Math.pow(x1, 2) + bPow * Math.pow(y1, 2)))
-                - e + f + g),
-            x_2 = calcX(y_2, x1, x2, y1, y2, r1, r2);
-
-        // Find the right point
-        double
-            dX1 = (x_1 - x3), dY1 = (y_1 - y3),
-            dX2 = (x_2 - x3), dY2 = (y_2 - y3);
-
-        double
-            rad1 = Math.sqrt((dX1 * dX1 + dY1 * dY1)),
-            rad2 = Math.sqrt((dX2 * dX2 + dY2 * dY2));
-
-        double
-            dR1 = Math.abs(r3 - rad1),
-            dR2 = Math.abs(r3 - rad2);
-
-        // DEBUG
-        /*
-        dummyDev1 = new Device("tes1", y_1, x_1);
-        dummyDev2 = new Device("tes2", y_2, x_2);
-        */
-
-        // Return
-        if(dR1 < dR2) return new Point(x_1, y_1);
-        return new Point(x_2, y_2);
+        return dist;
     }
 
     // Distance between (x1, y1) and (x2, y2)
@@ -257,9 +185,75 @@ public class CustomView extends View {
         return result;
     }
 
+    // Intersection Points between (x1, y1, r1) and (x2, y2, r2) circles
+    private Point[] getIntersectionPoints(double x1, double y1, double r1,
+                                        double x2, double y2, double r2) {
+        double
+            a = x1 * x1 - x2 * x2 - r1 * r1 + r2 * r2 + y1 * y1 - y2 * y2,
+            b = 2 * (x1 - x2),
+            c = y2 - y1,
+            bPow = Math.pow(b, 2),
+            cPow = Math.pow(c, 2),
+            d = bPow + 4 * cPow,
+            dd = 1 / (2 * d),
+            e = 4 * a * c,
+            f = 2 * bPow * y1,
+            g = 4 * b * c * x1,
+            h = 2 * a * b * x1,
+            i = Math.pow(a, 2),
+            j = bPow * Math.pow(r1, 2),
+            k = bPow * Math.pow(x1, 2),
+            l = bPow * Math.pow(y1, 2),
+            m = Math.pow((e - f - g), 2),
+            n = (i - h - j + k + l),
+            o = f + g - e,
+            p = Math.sqrt(m - 4 * d * n);
+
+        double
+            y_1 = dd * (o + p),
+            x_1 = calcX(y_1, x1, x2, y1, y2, r1, r2),
+
+            y_2 = dd * (o - p),
+            x_2 = calcX(y_2, x1, x2, y1, y2, r1, r2);
+
+        Point[] points = new Point[2];
+
+        points[0] = new Point(x_1, y_1);
+        points[1] = new Point(x_2, y_2);
+
+        return points;
+    }
+
     // Get x position from y
     private double calcX(double y, double x1, double x2, double y1, double y2, double r1, double r2) {
-        return (- r1 * r1 + r2 * r2 + x1 * x1 - x2 * x2 - 2 * y * y1 + 2 * y * y2 + y1 * y1 - y2 * y2) / (2*(x1 - x2));
+        double result = (- r1 * r1 + r2 * r2 + x1 * x1 - x2 * x2 - 2 * y * y1 + 2 * y * y2 + y1 * y1 - y2 * y2)
+                / (2*(x1 - x2));
+        return result;
+    }
+
+    // Get Right Point between 2 Intersection Points
+    private Point getTruePoint(Point[] points, double x, double y, double r) {
+        double
+            x1 = points[0].x, y1 = points[0].y,
+            x2 = points[1].x, y2 = points[1].y;
+
+        double
+            dX1 = (x1 - x), dY1 = (y1 - y),
+            dX2 = (x2 - x), dY2 = (y2 - y);
+
+        double
+            rad1 = Math.sqrt((dX1 * dX1 + dY1 * dY1)),
+            rad2 = Math.sqrt((dX2 * dX2 + dY2 * dY2));
+
+        double
+            dR1 = Math.abs(r - rad1),
+            dR2 = Math.abs(r - rad2);
+
+        if(dR1 < dR2) {
+            return points[0];
+        }
+
+        return points[1];
     }
 
     // Drawing Methods
@@ -282,48 +276,6 @@ public class CustomView extends View {
 
         canvas.drawCircle((float) cx, (float) cy, (float) r, paint);
     }
-
-    // DEBUG
-    /*
-    private void drawRadius(Canvas canvas) {
-        double cx, cy, r;
-        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-
-        if(drawRadius[2] != 0.0) {
-            cx = (112.804709 - topleftX) * scaleX;
-            cy = (topleftY + 7.286468) * scaleY;
-            r = drawRadius[2] * scaleX;
-            paint.setColor(Color.MAGENTA);
-            canvas.drawCircle((float) cx, (float) cy, (float) r, paint);
-        }
-
-        if(drawRadius[1] != 0.0) {
-            cx = (112.790550 - topleftX) * scaleX;
-            cy = (topleftY + 7.283147) * scaleY;
-            r = drawRadius[1] * scaleX;
-            paint.setColor(Color.CYAN);
-            canvas.drawCircle((float) cx, (float) cy, (float) r, paint);
-        }
-
-        if(drawRadius[0] != 0.0) {
-            cx = (112.800401 - topleftX) * scaleX;
-            cy = (topleftY + 7.274922) * scaleY;
-            r = drawRadius[0] * scaleX;
-
-            paint.setColor(Color.YELLOW);
-
-            canvas.drawCircle((float) cx, (float) cy, (float) r, paint);
-        }
-    }
-
-    private void drawIntersectionPoints(Canvas canvas) {
-        Paint dummyPaint = new Paint();
-        dummyPaint.setColor(Color.BLUE);
-
-        drawPin(canvas, dummyDev1.getLongitude(), dummyDev1.getLatitude(), dummyPaint);
-        drawPin(canvas, dummyDev2.getLongitude(), dummyDev2.getLatitude(), dummyPaint);
-    }
-    */
 
     // Public Methods
 
